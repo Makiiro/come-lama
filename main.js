f77e8e73a40e607cacf21bb34a29b10c215f8ef8
Makiiro <kiiromura@gmail.com>
2021-02-15 12:29:55 -0300

    proc

--- main.js
+++ main.js
@@ -13,25 +13,13 @@ client.on("message", message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLocaleLowerCase();
 
-    if (command === "comes") {
+    if (command === "comes" || command ==="come" || command ==="bebes" || command === "comecrit") {
         message.channel.send("TENHO PENA");
-    }
-
-    if (command === "come") {
-        message.channel.send("TENHO PENA");
-    }
-    
-    if (command === "bebes") {
-        message.channel.send("TENHO PENA");
-    }
-
-    if (command === "comecrit") {
-        message.channel.send("TENHO PENA");
-    }
+    };
 
     if (command === "matola") {
         message.channel.send("MATOLA NÃO COMA O MEU CRIT" + "<:bobsponga:445344728733581313><:bobsponga:445344728733581313><:bobsponga:445344728733581313><:bobsponga:445344728733581313><:bobsponga:445344728733581313><:bobsponga:445344728733581313><:bobsponga:445344728733581313><:bobsponga:445344728733581313><:bobsponga:445344728733581313><:bobsponga:445344728733581313>" );
-    }
+    };
 
     if (command === "inaba") {
         message.channel.send(`
@@ -45,7 +33,7 @@ client.on("message", message => {
 ...| . . . . . . . . . . ./.... . . . . . . . . . ./
 .....| . . . . . . . . ./....... . . . . . . . . /
 .....(. . . . . . . . . ......./. . . . . . . . . )`);
-    }
+    };
 
     if (command === "craque") {
         message.channel.send({files: ["https://i.imgur.com/6bKWryZ.png"]});
@@ -56,7 +44,7 @@ client.on("message", message => {
     };
 
     if (command === "mat") {
-        message.channel.send("Matola..? Pra onde está me levando? Por que estamos indo para o disk\nMatola..?\nPor que está mandando comes?\nO que você está faz.. aaahh Pare.. Matola.. por favor..AAAaaaaah.. Agora estou todo melado de criticos");
+        message.channel.send("Matola..? Pra onde está me levando? Por que estamos indo para o disk?\nMatola..?\nPor que está mandando comes?\nO que você está faz.. aaahh Pare.. Matola.. por favor..AAAaaaaah.. Agora estou todo melado de criticos");
     };
 });
 
